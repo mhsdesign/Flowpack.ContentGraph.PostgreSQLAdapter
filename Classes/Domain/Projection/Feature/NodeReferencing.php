6e1f199c0060c7495aa0c83b2e93bc9c65c3459b
Bernhard Schmitt <schmitt@sitegeist.de>
2022-04-01 12:05:13 +0200

    Enhance node referencing constraint checks

--- Classes/Domain/Projection/Feature/NodeReferencing.php
+++ Classes/Domain/Projection/Feature/NodeReferencing.php
@@ -15,6 +15,7 @@ declare(strict_types=1);
 namespace Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\Feature;
 
 use Doctrine\DBAL\Connection;
+use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\EventCouldNotBeAppliedToContentGraph;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\NodeAggregateIdentifiers;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\NodeRecord;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionHypergraph;
@@ -65,7 +66,7 @@ trait NodeReferencing
                     $referenceRelation->addToDatabase($this->getDatabaseConnection());
                 }
             } else {
-                // @todo log
+                throw EventCouldNotBeAppliedToContentGraph::becauseTheSourceNodeIsMissing(get_class($event));
             }
         });
     }
