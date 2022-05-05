b2497e0607625742fe03e8e5452935bca35c6303
Bernhard Schmitt <schmitt@sitegeist.de>
2022-05-05 16:33:52 +0200

    Adjust types

--- src/Domain/Projection/HypergraphProjector.php
+++ src/Domain/Projection/HypergraphProjector.php
@@ -25,8 +25,8 @@ use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\Feature\NodeRenami
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\Feature\NodeTypeChange;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\Feature\NodeVariation;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Infrastructure\DbalClient;
+use Neos\ContentRepository\Infrastructure\DbalClientInterface;
 use Neos\ContentRepository\Infrastructure\Projection\AbstractProcessedEventsAwareProjector;
-use Neos\ContentRepository\Service\Infrastructure\Service\DbalClient as EventStorageDbalClient;
 
 /**
  * The alternate reality-aware hypergraph projector for the PostgreSQL backend via Doctrine DBAL
@@ -49,7 +49,7 @@ final class HypergraphProjector extends AbstractProcessedEventsAwareProjector
 
     public function __construct(
         DbalClient $databaseClient,
-        EventStorageDbalClient $eventStorageDatabaseClient,
+        DbalClientInterface $eventStorageDatabaseClient,
         VariableFrontend $processedEventsCache
     ) {
         $this->databaseClient = $databaseClient;
