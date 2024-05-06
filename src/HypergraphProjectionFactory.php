e67c17be94e79af383e535472af1ab747d41803f
Christian Müller <christian@flownative.com>
2024-05-06 09:31:29 +0200

    TASK: ContentGraph bound to Workspace
    The ContentGraph is now always bound to a
    `WorkspaceName` and `ContentStreamId` combination.
    The ProjectionState of the ContentGraph is now
    the `ContentGraphFinder` which allows you to get
    an instance of the `ContentGraph` bound to a
    combination (usually by giving only the `WorkspaceName`).
    
    The userland way is to use
    
    `ContentRepository::getContentGraph(WorkspaceName)`

--- src/HypergraphProjectionFactory.php
+++ src/HypergraphProjectionFactory.php
@@ -7,6 +7,7 @@ namespace Flowpack\ContentGraph\PostgreSQLAdapter;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\HypergraphProjection;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Repository\NodeFactory;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Infrastructure\PostgresDbalClientInterface;
+use Neos\ContentRepository\Core\ContentGraphFinder;
 use Neos\ContentRepository\Core\Factory\ProjectionFactoryDependencies;
 use Neos\ContentRepository\Core\Projection\ProjectionFactoryInterface;
 use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId;
@@ -36,16 +37,16 @@ final class HypergraphProjectionFactory implements ProjectionFactoryInterface
             $projectionFactoryDependencies->contentRepositoryId
         );
 
-        return new HypergraphProjection(
-            $this->dbalClient,
-            new NodeFactory(
-                $projectionFactoryDependencies->contentRepositoryId,
-                $projectionFactoryDependencies->nodeTypeManager,
-                $projectionFactoryDependencies->propertyConverter
-            ),
+        $nodeFactory = new NodeFactory(
             $projectionFactoryDependencies->contentRepositoryId,
             $projectionFactoryDependencies->nodeTypeManager,
-            $tableNamePrefix
+            $projectionFactoryDependencies->propertyConverter
+        );
+
+        return new HypergraphProjection(
+            $this->dbalClient,
+            $tableNamePrefix,
+            new ContentGraphFinder(new ContentHyperGraphFactory($this->dbalClient, $nodeFactory, $projectionFactoryDependencies->contentRepositoryId, $projectionFactoryDependencies->nodeTypeManager, $tableNamePrefix))
         );
     }
 }
